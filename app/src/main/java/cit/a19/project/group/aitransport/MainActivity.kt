e0e2387e6d572ce704beeba153eafa8b97746ac8
murerr <rudy.murer@cansii.com>
2018-09-28 19:05:12 +0100

    Adding libraries & Dependencies

---
+++ app/src/main/java/cit/a19/project/group/aitransport/MainActivity.kt
@@ -0,0 +1,12 @@
+package cit.a19.project.group.aitransport
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
